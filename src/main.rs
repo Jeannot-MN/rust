2f3c4c941df9b243846a7eddb4cecc79e6e8fe66
JeannotMN <jeannot@ssquared.dev>
2022-11-03 22:02:51 +0200

    Generating random numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
@@ -7,11 +8,13 @@ fn main() {
 fn guess_the_number() {
     println!("*****Guess the number!**********");
 
+    let magic_number = rand::thread_rng().gen_range(1..=100);
     let mut guess = String::new();
     println!("Enter your guess: ");
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line!");
 
+    println!("The magic number is {magic_number}");
     println!("Your guess was {guess}");
 }
