c370ef9f0602c67148972c0872fec9e698c31567
JeannotMN <jeannot@ssquared.dev>
2022-11-03 12:00:27 +0200

    using std::io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    guess_the_number();
+}
+
+fn guess_the_number() {
+    println!("*****Guess the number!**********");
+
+    let mut guess = String::new();
+    println!("Enter your guess: ");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line!");
+
+    println!("Your guess was {guess}");
 }
