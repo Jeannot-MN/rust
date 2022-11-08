268e158792a16e02bd1210d8ee64121e3a51b0d7
JeannotMN <jeannot@ssquared.dev>
2022-11-08 20:12:13 +0200

    loop and break

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,27 @@ fn guess_the_number() {
     println!("*****Guess the number!**********");
 
     let magic_number = rand::thread_rng().gen_range(1..=100);
-    let mut guess = String::new();
-    println!("Enter your guess: ");
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
 
-    let guess: i32 = guess.trim().parse().expect("Please enter a number");
-    println!("The magic number is {magic_number}");
-    println!("Your guess was {guess}");
+    loop {
+        let mut guess = String::new();
+        println!("{guess}");
+        println!("Enter your guess: ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    match guess.cmp(&magic_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You got it!"),
+        let guess: i32 = guess.trim().parse().expect("Please enter a number");
+
+        println!("The magic number is {magic_number}");
+        println!("Your guess was {guess}");
+
+        match guess.cmp(&magic_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            },
+        }
     }
 }
